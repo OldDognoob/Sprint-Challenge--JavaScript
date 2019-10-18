b0d57af9164f395baef0590f63c15c6347d96574
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-10-18 12:41:36 +0300

    objects-arrays changes

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -81,6 +81,7 @@ const graduates = [
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
 
+graduates.map(obj)=>universities.push(`graduates${universities.sort}`);
 
 console.log(universities);
 
@@ -91,10 +92,16 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+
+graduates.forEach(obj)=> contactInfo.push(`graduates:${first_name} ${email}`);
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
+graduates.filter(obj)=>unisWithUni.push(`universities ${obj.Uni}`);
+
 console.log(unisWithUni);
 
 
